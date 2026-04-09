c0364790019161e2d9e6b8369f2695fff16bee7b
Sohit <sohitveersingh.1@gmail.com>
2026-04-09 22:18:06 +0530

    Configured Model Relations Ans Castings Created Enum Considering Different Status Still

---
+++ app/Enums/TodoStatusEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum TodoStatusEnum: string
+{
+    //
+}
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use App\Enums\TodoStatusEnum;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[Fillable(['title', 'description','status'])]
 class Todo extends Model
 {
-    //
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TodoStatusEnum::class,
+        ];
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
